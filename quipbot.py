1fedc4b362ae13bec6ad62e79ae1b544fd2b794c
markraub <markraub@markmac01.la.etc>
2019-04-11 15:56:45 -0700

    added PIL image processing with the prompts. Also tossed in a butt load of prompts, gotta start chopping those bad boys up

--- quipbot.py
+++ quipbot.py
@@ -1,4 +1,5 @@
-import tracery, json
+from PIL import Image, ImageDraw, ImageFont
+import tracery, json, string
 from tracery.modifiers import base_english
 
 grammar_in = ""
@@ -13,6 +14,28 @@ rules = json.load(open("grammar.json"))
 
 grammar = tracery.Grammar(rules)
 grammar.add_modifiers(base_english)
-print(grammar.flatten("#origin#"))
+quip = grammar.flatten("#origin#")
 
+img = Image.open("bg.png")
+fnt = ImageFont.truetype('/Lirary/Fonts/Georgia.ttf', 80)
+d = ImageDraw.Draw(img)
+x = 1280 
+y = 720
+shadowcolor = (0, 0, 0, 25)
+quip_l = list(str(quip))
+if d.textsize(quip, font=fnt)[0] > 2160:
+    for i in range(0, len(quip_l)):
+        if i > len(quip)/2:
+            if quip_l[i] == " ":
+                quip_l[i] = "\n"
+                break
+new_quip = string.join(quip_l, "")
+x = x - d.textsize(new_quip, font=fnt)[0] / 2 
+y = y - d.textsize(new_quip, font=fnt)[1] / 2
+
+d.multiline_text((x+2, y+2), new_quip, font=fnt, fill=shadowcolor, align="center")
+d.multiline_text((x, y), new_quip, font=fnt, fill="purple", align="center")
+img.save('out.png')
+print(new_quip)
+print(d.textsize(new_quip, font=fnt))
 
